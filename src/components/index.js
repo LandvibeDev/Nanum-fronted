b4b5cc0d1382e7bba0060a5d522d08876c5f68b4
KeonHee <kimgh6554@hanmail.net>
2016-07-25 21:35:32 +0900

    git ignore appled

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import App from './App/App.js';
-import Counter from './Counter/Counter.js';
-import Spinner from './Spinner/Spinner.js';
-import Issue from './Issue/Issue.js';
-
-export { App, Counter, Spinner , Issue};
